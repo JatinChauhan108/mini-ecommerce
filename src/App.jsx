5449d18ad449ae15bf259d89f4596ee9472bf9a3
Jatin Chauhan <jatinchauhan615@gmail.com>
2026-01-04 15:04:48 +0530

    cart button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import Products from './components/products';
+import Cart from './components/Cart';
 
 function App() {
 
+  const [page, setPage] = useState("products");
+
+  const handleCartClick = () => {
+    setPage("cart")
+  }
+  const handleProductsClick = () => {
+    setPage("products")
+  }
   return (
     <>
       <div>E-Commerce</div> 
-      <Products/>
+      <button onClick = {handleCartClick}>Go to Cart</button>
+      <button onClick = {handleProductsClick}>Go to Products</button>
+      { page === "products" ? <Products/> : <Cart /> }
     </>
   )
 }
